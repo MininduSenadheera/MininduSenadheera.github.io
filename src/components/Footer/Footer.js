6ef2d4b6e55362f71a777e6ad48bf4e9aca100e2
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-15 20:58:56 +0530

    created the footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer className="footer-area footer--light">
+            <div class="mini-footer">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="copyright-text">
+                      <p>© 2021. All rights reserved. Created by
+                        <a href="http://www.github.com/MininduSenadheera" target="_blank" rel="noreferrer"> Minindu Senadheera</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
