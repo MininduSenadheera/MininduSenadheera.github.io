1339b9565ddfc90ddde5411de6ea96eb0c700089
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-19 21:14:09 +0530

    added aos effects

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,29 +2,38 @@ import React from 'react'
 import './Home.css';
 import Typing from 'react-typing-animation';
 import { indigo } from '@mui/material/colors';
+import Aos from 'aos';
+import "aos/dist/aos.css";
 
 function Home() {
+    Aos.init({duration:1000})
+
     return (
-        <div className="container">
+        <div className="container " style={{ height: '100vh'}}>
             <div className="row">
-                <div className="col-xl-4">
-                    <div className="profile_img">
+                <div className="col-xl-5">
+                    <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+                    <div className="profile_img" data-aos="zoom-out">
                         <img src="/images/ProfilePic.jpg" className="rounded-circle" alt="profile pic"/>
                     </div>
                 </div>
-                <div className="col-xl-8 mt-5">
-                    <p>Hi there, I'm </p>
-                    <h1 style={{ color: indigo[700] }}>Minindu Senadheera</h1>
-                    <Typing>
-                        <p>
-                            Data Science Undergraduate | &nbsp; 
-                            <Typing.Delay ms={1000} />
-                            Full Stack Developer | &nbsp;
-                            <Typing.Delay ms={1000} />
-                            Photographer</p>
-                    </Typing>
-
-                    <div>
+                <div className="col-xl-7 mt-5">
+                    <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+                    <p data-aos="fade-up">Hi there, I'm </p>
+                    <h1 data-aos="fade-up" style={{ color: indigo[700] }}>Minindu Senadheera</h1>
+                    <div data-aos="fade-right" data-aos-delay="200">
+                       <Typing>
+                            <p>
+                                Data Science Undergraduate | &nbsp; 
+                                <Typing.Delay ms={500} />
+                                Full Stack Developer | &nbsp;
+                                <Typing.Delay ms={500} />
+                                Photographer</p>
+                        </Typing> 
+                    </div>
+                    
+                    <br/>
+                    <div data-aos="fade-right" data-aos-delay="200">
                         <a href="https://www.linkedin.com/in/minindusenadheera" target="_blank" rel="noreferrer">
                             <img src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin"/>
                         </a>
@@ -44,14 +53,11 @@ function Home() {
                         <a href="https://www.youtube.com/c/MininduSenadheera" target="_blank" rel="noreferrer">
                             <img src="https://img.icons8.com/color/48/000000/youtube-play.png" alt="youtube"/>
                         </a>
+                        <br/><br/>
+                        <button className="btn btn-md btn-primary">Download My CV</button>
                     </div>
-                    
-                    <br/>
-
-                    <button className="btn btn-md btn-primary">Download My CV</button>
                 </div>
-            </div>
-            
+            </div> 
         </div>
     )
 }
