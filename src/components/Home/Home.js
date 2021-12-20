58125dcbad22f7644582d568d5e6cb519115531f
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-20 16:08:29 +0530

    add hovering effect to social icons and removed hovering effect from profile pic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
     Aos.init({duration:1000})
 
     return (
-        <div className="container " style={{ height: '100vh'}}>
+        <div className="container mt-5 mb-5" style={{height: '90vh'}}>
             <div className="row">
                 <div className="col-xl-5">
                     <br/> <br/> <br/> <br/> <br/> <br/> <br/>
@@ -35,23 +35,23 @@ function Home() {
                     <br/>
                     <div data-aos="fade-right" data-aos-delay="200">
                         <a href="https://www.linkedin.com/in/minindusenadheera" target="_blank" rel="noreferrer">
-                            <img src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin"/>
+                            <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin"/>
                         </a>
                         &nbsp;
                         <a href="https://github.com/MininduSenadheera" target="_blank" rel="noreferrer">
-                            <img src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github"/>
+                            <img className="social-icons" src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github"/>
                         </a>
                         &nbsp;
                         <a href="https://www.instagram.com/minindu_senadheera/" target="_blank" rel="noreferrer">
-                            <img src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram"/>
+                            <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram"/>
                         </a>
                         &nbsp;
                         <a href="https://www.facebook.com/minindu.senadheera/" target="_blank" rel="noreferrer">
-                            <img src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook"/>
+                            <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook"/>
                         </a>
                         &nbsp;
                         <a href="https://www.youtube.com/c/MininduSenadheera" target="_blank" rel="noreferrer">
-                            <img src="https://img.icons8.com/color/48/000000/youtube-play.png" alt="youtube"/>
+                            <img className="social-icons" src="https://img.icons8.com/color/48/000000/youtube-play.png" alt="youtube"/>
                         </a>
                         <br/><br/>
                         <button className="btn btn-md btn-primary">Download My CV</button>
