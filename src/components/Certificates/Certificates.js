346798329ecba807ed02b3dae5ee2bcb66b6c425
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-23 12:31:25 +0530

    certificates section completed

---
+++ src/components/Certificates/Certificates.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { indigo } from '@mui/material/colors';
+import './Certificates.css'
+import Aos from 'aos';
+import "aos/dist/aos.css";
+
+function Certificates() {
+    Aos.init({duration:1000, once:true})
+
+    return (
+        <div id="certificates" class="container work">
+            <h2 style={{ color: indigo[500], fontWeight: 700 }} data-aos="fade-up">My Achievements</h2>
+            <main class="page-content mt-5">
+                <div class="img-hover-zoom"  data-aos="fade-up">
+                    <img src="/certificates/Udemy Automate boring stuff with python.jpg" width="400px" height="300px" alt="Udemy Automate Python "/>
+                </div>
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
+                    <img src="/certificates/Great Learning Python for machine learning.jpg" width="400px" height="300px" alt="GL python ML "/>
+                </div>
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
+                    <img src="/certificates/One Month Learn JavaScript Certificate.jpg" width="400px" height="300px" alt="One month js "/>
+                </div>
+                <div class="img-hover-zoom"  data-aos="fade-up">
+                    <img src="/certificates/One Month Learn jQuery Certificate.jpg" width="400px" height="300px" alt="One month jquery"/>
+                </div>
+                <div class="img-hover-zoom" data-aos="fade-up" data-aos-delay="200">
+                    <img src="/certificates/One Month Learn SQL Certificate.jpg" width="400px" height="300px" alt="One month sql"/>
+                </div>
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
+                    <img src="/certificates/One Month Learn WordPress  Certificate.jpg" width="400px" height="300px" alt="One month wordpress"/>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Certificates
