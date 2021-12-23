39ac42ab3acd6a26c770a3ceba3f90a9c918aa92
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-23 12:34:35 +0530

    added the routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header'
+import Home from './components/Home/Home';
+import Footer from './components/Footer/Footer';
+import Projects from './components/Projects/Projects';
+import Certificates from './components/Certificates/Certificates';
 
 function App() {
   return (
     <div className="App">
-      <h1>Under construction</h1>
+      <Router>
+        <div>
+          <Header />
+          <Home />
+          <Projects />
+          <Certificates />
+          <Footer />
+        </div>
+      </Router>
     </div>
   );
 }
