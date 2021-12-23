f101ac42127fba6de1dbe8cb9190a3a45e9de73e
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2021-12-23 12:32:13 +0530

    header completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Header.css';
+import { Link } from "react-scroll";
+
+function Header() {
+    
+    return (
+        <header>
+            <nav className="navbar navbar-inverse navbar-expand-lg navbar-light fixed-top header-bg">
+                <div className="container">
+                    <div></div>
+                    <div class="hexagon-item">
+                        <div class="hex-content">
+                            <div class="hex-content-inner">
+                                <div class="title">
+                                    <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
+                                        Home
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hexagon-item">
+                        <div class="hex-content">
+                            <div class="hex-content-inner">
+                                <div class="title">
+                                    <Link to="about" spy={true} smooth={true} offset={50} duration={500}>
+                                        About
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hexagon-item">
+                        <div class="hex-content">
+                            <div class="hex-content-inner">
+                                <div class="title">
+                                    <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
+                                        Projects
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="hexagon-item">
+                        <div class="hex-content">
+                            <div class="hex-content-inner">
+                                <div class="title">
+                                    <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
+                                        Certifications
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
