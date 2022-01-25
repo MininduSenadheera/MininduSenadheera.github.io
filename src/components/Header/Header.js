be6afed194d41f1a6ead87b6629868fe45c2924c
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-01-25 16:06:12 +0530

    removed unwanted classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,50 +6,42 @@ function Header() {
     
     return (
         <header>
-            <nav className="navbar navbar-inverse navbar-expand-lg navbar-light fixed-top header-bg">
+            <nav className="navbar navbar-expand-lg fixed-top header-bg py-4">
                 <div className="container">
                     <div></div>
-                    <div class="hexagon-item">
-                        <div class="hex-content">
-                            <div class="hex-content-inner">
-                                <div class="title">
-                                    <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
-                                        Home
-                                    </Link>
-                                </div>
+                    <div>
+                        <div class="hex-content-inner">
+                            <div class="title">
+                                <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
+                                    Home
+                                </Link>
                             </div>
                         </div>
                     </div>
-                    <div class="hexagon-item">
-                        <div class="hex-content">
-                            <div class="hex-content-inner">
-                                <div class="title">
-                                    <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
-                                        About
-                                    </Link>
-                                </div>
+                    <div>
+                        <div class="hex-content-inner">
+                            <div class="title">
+                                <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
+                                    About
+                                </Link>
                             </div>
                         </div>
                     </div>
-                    <div class="hexagon-item">
-                        <div class="hex-content">
-                            <div class="hex-content-inner">
-                                <div class="title">
-                                    <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
-                                        Projects
-                                    </Link>
-                                </div>
+                    <div>
+                        <div class="hex-content-inner">
+                            <div class="title">
+                                <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
+                                    Projects
+                                </Link>
                             </div>
                         </div>
                     </div>
-                    <div class="hexagon-item">
-                        <div class="hex-content">
-                            <div class="hex-content-inner">
-                                <div class="title">
-                                    <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
-                                        Certifications
-                                    </Link>
-                                </div>
+                    <div>
+                        <div class="hex-content-inner">
+                            <div class="title">
+                                <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
+                                    Certifications
+                                </Link>
                             </div>
                         </div>
                     </div>
