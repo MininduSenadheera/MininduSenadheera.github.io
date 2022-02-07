9355594263c2af991019f4c1a7642a6db1b100ff
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-02-08 00:03:35 +0530

    image size responsive

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -12,22 +12,22 @@ function Certificates() {
             <h2 style={{ color: indigo[500], fontWeight: 700 }} data-aos="fade-up">My Achievements</h2>
             <main class="page-content mt-5">
                 <div class="img-hover-zoom"  data-aos="fade-up">
-                    <img src="/certificates/Udemy Automate boring stuff with python.jpg" width="400px" height="300px" alt="Udemy Automate Python "/>
+                    <img src="/certificates/Udemy Automate boring stuff with python.jpg" className='img-fluid certificate' alt="Udemy Automate Python "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
-                    <img src="/certificates/Great Learning Python for machine learning.jpg" width="400px" height="300px" alt="GL python ML "/>
+                    <img src="/certificates/Great Learning Python for machine learning.jpg" className='img-fluid certificate' alt="GL python ML "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
-                    <img src="/certificates/One Month Learn JavaScript Certificate.jpg" width="400px" height="300px" alt="One month js "/>
+                    <img src="/certificates/One Month Learn JavaScript Certificate.jpg" className='img-fluid certificate' alt="One month js "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up">
-                    <img src="/certificates/One Month Learn jQuery Certificate.jpg" width="400px" height="300px" alt="One month jquery"/>
+                    <img src="/certificates/One Month Learn jQuery Certificate.jpg" className='img-fluid certificate' alt="One month jquery"/>
                 </div>
                 <div class="img-hover-zoom" data-aos="fade-up" data-aos-delay="200">
-                    <img src="/certificates/One Month Learn SQL Certificate.jpg" width="400px" height="300px" alt="One month sql"/>
+                    <img src="/certificates/One Month Learn SQL Certificate.jpg" className='img-fluid certificate' alt="One month sql"/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
-                    <img src="/certificates/One Month Learn WordPress  Certificate.jpg" width="400px" height="300px" alt="One month wordpress"/>
+                    <img src="/certificates/One Month Learn WordPress  Certificate.jpg" className='img-fluid certificate' alt="One month wordpress"/>
                 </div>
             </main>
         </div>
