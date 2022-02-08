6872681ee5639107ad6cee964380cbaf3bdba5bc
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-02-08 19:54:46 +0530

    responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,52 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
+import DehazeIcon from '@mui/icons-material/Dehaze';
 import { Link } from "react-scroll";
 
 function Header() {
+
+    const [toggleMenu, setToggleMenu] = useState(false)
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+      
+        window.addEventListener('resize', changeWidth)
+
+
+        return () => {
+            window.removeEventListener('resize', changeWidth)
+        }
+    }, []);
+    
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
     
     return (
         <header>
             <nav className="navbar navbar-expand-lg fixed-top header-bg py-4">
+                {(toggleMenu || screenWidth > 500) && (
                 <div className="container">
                     <div></div>
                     <div>
-                        <div class="hex-content-inner">
-                            <div class="title">
-                                <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
-                                    Home
-                                </Link>
-                            </div>
+                        <div class="title">
+                            <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
+                                Home
+                            </Link>
                         </div>
                     </div>
                     <div>
-                        <div class="hex-content-inner">
-                            <div class="title">
-                                <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
-                                    About
-                                </Link>
-                            </div>
+                        <div class="title">
+                            <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
+                                About
+                            </Link>
                         </div>
                     </div>
                     <div>
-                        <div class="hex-content-inner">
-                            <div class="title">
-                                <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
-                                    Projects
-                                </Link>
-                            </div>
+                        <div class="title">
+                            <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
+                                Projects
+                            </Link>
                         </div>
                     </div>
-                    <div>
-                        <div class="hex-content-inner">
-                            <div class="title">
-                                <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
-                                    Certifications
-                                </Link>
-                            </div>
+                    <div >
+                        <div class="title">
+                            <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
+                                Certifications
+                            </Link>
                         </div>
                     </div>
                     <div></div>
                 </div>
+                )}
+
+                <span onClick={toggleNav} className="toggle"><DehazeIcon/></span>
             </nav>
         </header>
     )
