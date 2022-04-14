251ca2407e62e98e8568a1074cfdb17c5c78ecd5
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-04-14 13:34:41 +0530

    grid layout updated

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -8,29 +8,29 @@ function Certificates() {
     Aos.init({duration:1000, once:true})
 
     return (
-        <div id="certificates" class="container work">
+        <div id="certificates" class="container grid">
             <h2 style={{ color: indigo[500], fontWeight: 700 }} data-aos="fade-up">Online Achievements</h2>
             <main class="page-content mt-5">
                 <div class="img-hover-zoom"  data-aos="fade-up">
-                    <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/46213604" className='img-fluid certificate' alt="Hacktitude 99x 2022 "/>
+                    <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/46213604" className='img-fluid' alt="Hacktitude 99x 2022 "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up">
-                    <img src="/certificates/Udemy Automate boring stuff with python.jpg" className='img-fluid certificate' alt="Udemy Automate Python "/>
+                    <img src="/certificates/Udemy Automate boring stuff with python.jpg" className='img-fluid' alt="Udemy Automate Python "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
-                    <img src="/certificates/Great Learning Python for machine learning.jpg" className='img-fluid certificate' alt="GL python ML "/>
+                    <img src="/certificates/Great Learning Python for machine learning.jpg" className='img-fluid' alt="GL python ML "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
-                    <img src="/certificates/One Month Learn JavaScript Certificate.jpg" className='img-fluid certificate' alt="One month js "/>
+                    <img src="/certificates/One Month Learn JavaScript Certificate.jpg" className='img-fluid' alt="One month js "/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up">
-                    <img src="/certificates/One Month Learn jQuery Certificate.jpg" className='img-fluid certificate' alt="One month jquery"/>
+                    <img src="/certificates/One Month Learn jQuery Certificate.jpg" className='img-fluid' alt="One month jquery"/>
                 </div>
                 <div class="img-hover-zoom" data-aos="fade-up" data-aos-delay="200">
-                    <img src="/certificates/One Month Learn SQL Certificate.jpg" className='img-fluid certificate' alt="One month sql"/>
+                    <img src="/certificates/One Month Learn SQL Certificate.jpg" className='img-fluid' alt="One month sql"/>
                 </div>
                 <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
-                    <img src="/certificates/One Month Learn WordPress  Certificate.jpg" className='img-fluid certificate' alt="One month wordpress"/>
+                    <img src="/certificates/One Month Learn WordPress  Certificate.jpg" className='img-fluid' alt="One month wordpress"/>
                 </div>
             </main>
         </div>
