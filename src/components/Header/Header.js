b2ade46f82497b8df952297734c06a062272ce1e
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-05-03 15:32:50 +0530

    changed the navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,71 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './Header.css';
-import DehazeIcon from '@mui/icons-material/Dehaze';
 import { Link } from "react-scroll";
 
+
 function Header() {
-
-    const [toggleMenu, setToggleMenu] = useState(false)
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-
-    useEffect(() => {
-        const changeWidth = () => {
-            setScreenWidth(window.innerWidth);
-        }
-      
-        window.addEventListener('resize', changeWidth)
-
-
-        return () => {
-            window.removeEventListener('resize', changeWidth)
-        }
-    }, []);
-    
-    const toggleNav = () => {
-        setToggleMenu(!toggleMenu)
-    }
     
     return (
-        <header>
-            <nav className="navbar navbar-expand-lg fixed-top header-bg py-4">
-                {(toggleMenu || screenWidth > 500) && (
-                <div className="container">
-                    <div></div>
-                    <div>
-                        <div class="title">
-                            <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>
-                                Home
-                            </Link>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="title">
+        <nav class="navbar navbar-expand-lg navbar-default navbar-light navbar-custom fixed-top">
+            <div class="container">
+                {/* Logo */}
+                <a class="navbar-brand logo" href="home">
+                    <h4>Minindu</h4>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="nav navbar-nav ms-auto navbar-center" id="mySidenav">
+                        <li class="nav-item">
+                            <Link to="home" spy={true} smooth={true} offset={-100} duration={500}>Home</Link>
+                        </li>
+                        <li class="nav-item">
                             <Link to="about" spy={true} smooth={true} offset={-100} duration={500}>
                                 About
                             </Link>
-                        </div>
-                    </div>
-                    <div>
-                        <div class="title">
+                        </li>
+                        <li class="nav-item">
                             <Link to="projects" spy={true} smooth={true} offset={-90} duration={500}>
                                 Projects
                             </Link>
-                        </div>
-                    </div>
-                    <div >
-                        <div class="title">
+                        </li>
+                        <li class="nav-item">
                             <Link to="certificates" spy={true} smooth={true} offset={-100} duration={500}>
                                 Certifications
                             </Link>
-                        </div>
-                    </div>
-                    <div></div>
+                        </li>
+                    </ul>
                 </div>
-                )}
-
-                <span onClick={toggleNav} className="toggle"><DehazeIcon/></span>
-            </nav>
-        </header>
+            </div>
+        </nav>
     )
 }
 
