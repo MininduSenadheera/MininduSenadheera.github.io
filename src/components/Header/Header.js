f261c3491c99e10ddb60e5833cc13fe1c60f49b2
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-05-04 23:54:23 +0530

    remove unwanted classes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,7 @@ function Header() {
     return (
         <nav class="navbar navbar-expand-lg navbar-default navbar-light navbar-custom fixed-top">
             <div class="container-fluid">
-                <Link class="navbar-brand logo" to="home">
+                <Link class="navbar-brand" to="home">
                     <img src='/images/Mlogo.png' className='logo' alt='logo'/>
                 </Link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
