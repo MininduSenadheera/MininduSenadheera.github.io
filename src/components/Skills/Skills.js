a6c51cf981743507cd900221163db3ae93bbb89e
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-05-24 21:39:53 +0530

    skill bars added

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './Skills.css'
+import VisibilitySensor from "react-visibility-sensor";
 
 function Skills() {
+
     return (
         <section id='skills' className="section">
             <div className='container' align="center">
                 <div className='row'>
-                    <div className='col-xl-6  mb-5'>
+                    <div className='col-xl-5  mb-5'>
                         <div className='boxAbout'>
                             <h3 className='subtitle mb-4'>Languages</h3>
 
@@ -51,9 +55,141 @@ function Skills() {
                             </div>
                         </div>
                     </div>
-                    <div className='col-xl-6  mb-5'>
-                        {/* <h3 className='subtitle'>Skills</h3> */}
+                    <div className='col-xl-7  mb-5'>
+                        <h3 className='subtitle mb-3'>Professional Skills</h3>
                         <div className='skillGrid'>
+                            <div className='boxSkill' >
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 90 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Communication</h5>
+                            </div>
+                            <div className='boxSkill'>
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 80 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Creativity</h5>
+                            </div>
+                            <div className='boxSkill'>
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 95 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Team Work</h5>
+                            </div>
+                            <div className='boxSkill'>
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 75 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Leadership</h5>
+                            </div>
+                            <div className='boxSkill'>
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 90 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Problem Solving</h5>
+                            </div>
+                            <div className='boxSkill'>
+                                <VisibilitySensor>
+                                    {({ isVisible }) => {
+                                        const percentage = isVisible ? 80 : 0;
+                                        return (
+                                            <CircularProgressbar
+                                                value={percentage}
+                                                text={`${percentage}%`}
+                                                styles={buildStyles({
+                                                    pathTransitionDuration: 1.5,
+                                                    // Colors
+                                                    pathColor: '#5f86f2',
+                                                    trailColor: '#e9ecef',
+                                                    textColor: '#5f86f2',
+                                                })}
+                                            />
+                                        );
+                                    }}
+                                </VisibilitySensor>
+
+                                <h5 className='mt-3 mb-0' align='center'>Positive Attitude</h5>
+                            </div>
                             
                         </div>
                     </div>
