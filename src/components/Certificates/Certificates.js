2fc4aee0e76edac65437993c3e4b635f212c23ca
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-05-25 17:06:45 +0530

    added azure badge

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -10,31 +10,34 @@ function Certificates() {
         <div id="certificates" class="container grid">
             <h1 className="title" data-aos="fade-up">Online Achievements</h1>
             <main class="page-content-certificates mt-5">
-                <div class="img-hover-zoom"  data-aos="fade-up">
+                <div class="badge-hover-zoom"  data-aos="fade-up" align="center">
+                    <img src="/certificates/microsoft-certified-azure-fundamentals.png" className='img-fluid' alt="Azure fundementals badge "/>
+                </div>
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
                     <img src="/certificates/Azure Fundamentals.png" className='img-fluid' alt="Azure fundementals badge "/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
                     <img src="/certificates/Python_for_Beginners_E-Certificate.jpg" className='img-fluid' alt="UOM Python"/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
+                <div class="img-hover-zoom"  data-aos="fade-up">
                     <img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/certificate/46213604" className='img-fluid' alt="Hacktitude 99x 2022 "/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up"  >
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200" >
                     <img src="/certificates/Udemy Automate boring stuff with python.jpg" className='img-fluid' alt="Udemy Automate Python "/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200">
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
                     <img src="/certificates/Great Learning Python for machine learning.jpg" className='img-fluid' alt="GL python ML "/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
+                <div class="img-hover-zoom"  data-aos="fade-up">
                     <img src="/certificates/One Month Learn JavaScript Certificate.jpg" className='img-fluid' alt="One month js "/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" >
+                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="200" >
                     <img src="/certificates/One Month Learn jQuery Certificate.jpg" className='img-fluid' alt="One month jquery"/>
                 </div>
-                <div class="img-hover-zoom" data-aos="fade-up" data-aos-delay="200">
+                <div class="img-hover-zoom" data-aos="fade-up" data-aos-delay="300">
                     <img src="/certificates/One Month Learn SQL Certificate.jpg" className='img-fluid' alt="One month sql"/>
                 </div>
-                <div class="img-hover-zoom"  data-aos="fade-up" data-aos-delay="300">
+                <div class="img-hover-zoom"  data-aos="fade-up">
                     <img src="/certificates/One Month Learn WordPress  Certificate.jpg" className='img-fluid' alt="One month wordpress"/>
                 </div>
             </main>
