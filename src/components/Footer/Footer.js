38bc9e34c39c6604f12904dc0009421a04c6e535
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-07-10 02:02:06 +0530

    site visit counter

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import countapi from 'countapi-js';
 import './Footer.css'
 
 function Footer() {
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    countapi.visits('global').then((result) => {
+      setCount(result.value);
+    });
+  }, [])
+
     return (
       <footer className='mt-5'>
-        <div class="copyright-text">
-          <p>© 2021. All rights reserved.
+        <h6>Page visits: {count}</h6>
+        <div className="copyright-text">
+          <p>All rights reserved.
             <br></br>
             Created by
             <a href="http://www.github.com/MininduSenadheera" target="_blank" rel="noreferrer"> Minindu Senadheera</a>
