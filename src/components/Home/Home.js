fa1bc5a19b85b16ffa0f9e3da336cbf73763419a
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2023-10-08 00:27:12 +0530

    optimise landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,73 +1,57 @@
 import React from 'react'
 import './Home.css';
-import Typing from 'react-typing-animation';
+import { TypeAnimation } from 'react-type-animation';
 import Aos from 'aos';
 import "aos/dist/aos.css";
-import { Link } from 'react-router-dom';
 
 function Home() {
-    Aos.init({duration:1000})
+	Aos.init({ duration: 1000 })
 
-    return (
-        <section id="home" className="section home d-flex align-items-center">
-            <div className='container'>
-                {/* <div className='row my-5 bg-danger'>
-                    <div className='col-12 text-light text-center'>
-                        <h1>This site is still under construction</h1>
-                    </div>
-                </div> */}
-                <div className="row">
-                    <div className="col-xl-5">
-                        <div data-aos="zoom-out" align="center">
-                            <img src="/images/ProfilePic.jpg" className="circle-shadow profile img-fluid" alt="profile pic"/>
-                        </div>
-                    </div>
-                    <div className="col-xl-7 d-flex flex-column justify-content-center">
-                        <p data-aos="fade-up">Hi there, I'm </p>
-
-                        <h1 className="userName my-0">Minindu Senadheera</h1>
-                        
-                        <div className='mt-2' data-aos="fade-right" data-aos-delay="200">
-                            <Typing>
-                                <p>
-                                    Associate Data Scientist | &nbsp; 
-                                    <Typing.Delay ms={500} />
-                                    Full Stack Developer | &nbsp;
-                                    <Typing.Delay ms={500} />
-                                    Photographer</p>
-                            </Typing> 
-                        </div>
-                        
-                        <br/>
-                        <div data-aos="fade-right" data-aos-delay="200">
-                            <a href="mailto:minindusenadheera@gmail.com">
-                                <img className="social-icons" src="https://img.icons8.com/fluency/48/000000/apple-mail.png" alt="linkedin"/>
-                            </a>
-                            <a href="https://www.linkedin.com/in/minindusenadheera" target="_blank" rel="noreferrer">
-                                <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin"/>
-                            </a>
-                            &nbsp;
-                            <a href="https://github.com/MininduSenadheera" target="_blank" rel="noreferrer">
-                                <img className="social-icons" src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github"/>
-                            </a>
-                            &nbsp;
-                            <a href="https://www.instagram.com/minindu_senadheera/" target="_blank" rel="noreferrer">
-                                <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram"/>
-                            </a>
-                            &nbsp;
-                            <a href="https://www.facebook.com/minindu.senadheera/" target="_blank" rel="noreferrer">
-                                <img className="social-icons" src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook"/>
-                            </a>
-                            <br/><br/>
-                            <Link hidden="true" to="/pdf/Resume - Minindu Senadheera.pdf" target="_blank" download>
-                                <button className="btn btn-md btn-primary">Download My CV</button>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div> 
-        </section>
-    )
+	return (
+		<section id="home" className="container vh-100 d-flex justify-content-center align-items-center">
+			<div className='d-flex align-items-center flex-wrap'>
+				<div className='p-5' data-aos="zoom-out" align="center">
+					<img src="/images/ProfilePic.jpg" className="home-img img-fluid" alt="profile pic" />
+				</div>
+				<div>
+					<p data-aos="fade-up">Hi there, I'm </p>
+					<h1 className="userName display-5 fw-normal">Minindu Senadheera</h1>
+					<p data-aos="fade-right" data-aos-delay="200">
+						<TypeAnimation
+							sequence={[
+								'Data Scientist',
+								500,
+								'Data Scientist | Software Engineer',
+								500,
+								'Data Scientist | Software Engineer | Traveller'
+							]}
+						/>
+					</p>
+					<br />
+					<div data-aos="fade-right" data-aos-delay="200">
+						<a href="mailto:minindusenadheera@gmail.com">
+							<img className="social-icons" src="https://img.icons8.com/fluency/48/000000/apple-mail.png" alt="linkedin" />
+						</a>
+						<a href="https://www.linkedin.com/in/minindusenadheera" target="_blank" rel="noreferrer">
+							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin" />
+						</a>
+						&nbsp;
+						<a href="https://github.com/MininduSenadheera" target="_blank" rel="noreferrer">
+							<img className="social-icons" src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github" />
+						</a>
+						&nbsp;
+						<a href="https://www.instagram.com/minindu_senadheera/" target="_blank" rel="noreferrer">
+							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram" />
+						</a>
+						&nbsp;
+						<a href="https://www.facebook.com/minindu.senadheera/" target="_blank" rel="noreferrer">
+							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
 
 export default Home
