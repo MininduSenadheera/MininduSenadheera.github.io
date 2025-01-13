631ba152bbc52ac0a5adf8fb8b1914199e5a479a
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-14 01:40:02 +0530

    add phone link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -29,23 +29,26 @@ function Home() {
 					</p>
 					<br />
 					<div data-aos="fade-right" data-aos-delay="200">
+						<a href='tel:+94767041198'>
+							<img src="https://img.icons8.com/?&id=I24lanX6Nq71&format=png&color=000000" alt="phone" width={48} />
+						</a>
 						<a href="mailto:minindusenadheera@gmail.com">
-							<img className="social-icons" src="https://img.icons8.com/fluency/48/000000/apple-mail.png" alt="linkedin" />
+							<img src="https://img.icons8.com/fluency/48/000000/apple-mail.png" alt="linkedin" width={48} />
 						</a>
 						<a href="https://www.linkedin.com/in/minindusenadheera" target="_blank" rel="noreferrer">
-							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin" />
+							<img src="https://img.icons8.com/fluency/50/000000/linkedin.png" alt="linkedin" width={48} />
 						</a>
 						&nbsp;
 						<a href="https://github.com/MininduSenadheera" target="_blank" rel="noreferrer">
-							<img className="social-icons" src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github" />
+							<img src="https://img.icons8.com/ios-glyphs/48/000000/github.png" alt="github" width={48} />
 						</a>
 						&nbsp;
 						<a href="https://www.instagram.com/minindu_senadheera/" target="_blank" rel="noreferrer">
-							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram" />
+							<img src="https://img.icons8.com/fluency/50/000000/instagram-new.png" alt="instagram" width={48} />
 						</a>
 						&nbsp;
 						<a href="https://www.facebook.com/minindu.senadheera/" target="_blank" rel="noreferrer">
-							<img className="social-icons" src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook" />
+							<img src="https://img.icons8.com/fluency/50/000000/facebook-new.png" alt="facebook" width={48} />
 						</a>
 					</div>
 				</div>
