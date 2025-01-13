cb8dba575b60bb3daf36822bcf245973dfa6520b
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-01-14 01:54:16 +0530

    about section updated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -21,19 +21,16 @@ function About() {
                             </h5>
                             <br></br>
                             <p data-aos="fade-up" data-aos-delay="100">
-                                🎓 I recently graduated with a Second Upper Class Honours in BSc (Hons) Information Technology specializing in Data Science from the prestigious Sri Lanka Institute of Information Technology. Throughout my studies, I consistently excelled, even achieving a GPA exceeding 3.7 and landing on the Dean's List. My academic journey began at S. Thomas' College Mount Lavinia, where I delved into the world of Physical Sciences.
+                                🎓 I graduated in 2024 with a Second Class Honours in BSc (Hons) Information Technology specializing in Data Science from the prestigious Sri Lanka Institute of Information Technology. Throughout my studies, I consistently excelled, even achieving a GPA exceeding 3.7 and landing on the Dean's List. My academic journey began at S. Thomas' College Mount Lavinia, where I delved into the world of Physical Sciences.
                             </p>
                             <p data-aos="fade-up" data-aos-delay="200">
-                                💼 By day, I'm a dynamic Data Scientist and Full-stack Software Engineer at Sling Mobility (PVT) Ltd, an innovative Energy-as-a-Service company for electric vehicles (EVs).
+                                💼 By day, I'm a Full-stack Software Engineer at PSHA (Private Sector Humanitarian Alliance), an organisation at the forefront of redefining corporate engagement in in global Humanitarian efforts.
                             </p>
                             <p data-aos="fade-up" data-aos-delay="300">
-                                🔍 My passions? Data Science, Artificial Intelligence, and Machine Learning! I thrive on being a fast learner with a knack for problem-solving. I love staying at the forefront of technology, continually adding new programming languages and tools to my arsenal. Keeping up with global tech trends is my jam.
+                                🔍 My passions? Full Stack Software Engineering, Web Development, App development, Data Science, Artificial Intelligence, and Machine Learning! I thrive on being a fast learner with a knack for problem-solving. I love staying at the forefront of technology, continually adding new programming languages and tools to my arsenal. Keeping up with global tech trends is my jam.
                             </p>
                             <p data-aos="fade-up" data-aos-delay="400">
                                 ✈️ When I'm not crunching data or writing code, you'll find me exploring new horizons through my love for travel and photography.
-                            </p>
-                            <p data-aos="fade-up" data-aos-delay="500">
-                                Let's navigate the tech world together and turn data into meaningful insights!
                             </p>
                         </div>
                     </div>
