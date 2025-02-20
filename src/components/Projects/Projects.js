a16622183224b6e946dd2dd3b790871343e62033
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-02-20 22:40:55 +0530

    new projects structure

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import LanguageIcon from '@mui/icons-material/Language';
-import GitHubIcon from '@mui/icons-material/GitHub';
 import './Projects.css'
 import Aos from 'aos';
 import "aos/dist/aos.css";
@@ -9,130 +8,86 @@ import "aos/dist/aos.css";
 function Projects() {
     Aos.init({ duration: 1000, once: false })
 
+    const projects = [{
+        title: "Artigala Ayurveda",
+        description: "E-commerce website for Ayurveda store. With payment gateways, pay later options, digital invoices, admin panel and order management.",
+        img: "/images/ArtigalaAyurveda.png",
+        link: "https://artigalaayurveda.com"
+    }, {
+        title: "Ashen Ravisara Portfolio",
+        description: "Portfolio Website for an UI/UX Designer.",
+        img: "/images/Ashen.png",
+        link: "https://ashen-ravisara.github.io"
+    }, {
+        title: "FieldR",
+        description: "Online cricket analytics and digital score sheet mobile app.",
+        img: "/images/fieldr.png",
+        link: "https://community.fieldr.lk"
+    }, {
+        title: "slingmobility.com",
+        description: "Affordable and innovative 360 degree EV solutions and fleet management.",
+        img: "/images/sling.png",
+        link: "https://slingmobility.com"
+    }, {
+        title: "aenigm3labs.com",
+        description: "Aenigme provides digital strategies for products & small and medium enterprises.",
+        img: "/images/aenigme.png",
+        link: "https://aenigm3labs.com"
+    }, {
+        title: "Airbnb Price predictor",
+        description: "Predicting price of Airbnb hotel listings in Boston based on facilities provided.",
+        img: "/images/Airbnb Price Predictor.png",
+        link: "https://airbnb-price-prediction.streamlitapp.com"
+    }, {
+        title: "DWBI for Airbnb Boston",
+        description: "Data Warehousing and Business Intelligence project done for Airbnb hotel listings in Boston.",
+        img: "/images/DWBIAirbnb.png",
+        link: "https://github.com/MininduSenadheera/DWBI-Airbnb-Boston"
+    }, {
+        title: "DWBI for Grocery shop",
+        description: "Data Warehousing and Business Intelligence project done for a grocery shop.",
+        img: "/images/DWBIAirbnb.png",
+        link: "'https://github.com/MininduSenadheera/DWBI-Grocery-Shop"
+    }, {
+        title: "Aspirus Health Care Web Application",
+        description: "Website for channelling doctors, e-prescriptions, telemedicine with live video audio conference and pharmacy store.",
+        img: "/images/AspirusHealthCare.png",
+        link: "https://github.com/JMAT-Technologies/Aspirus-Health-Care-WebApp"
+    }, {
+        title: "Aspirus Health Care Android App",
+        description: "Android app for channelling doctors, e-prescriptions, telemedicine with live video audio conference and pharmacy store.",
+        img: "/images/AspirusAndroid.png",
+        link: "https://github.com/JMAT-Technologies/Aspirus-Health-Care-Android-App"
+    }, {
+        title: "Bus Ticket Reservation System",
+        description: "A bus ticket reservation application where users can reserve seats online and manage their reservations.",
+        img: "/images/BusTicketReservation.png",
+        link: "https://github.com/MininduSenadheera/Bus-Ticket-Reservation"
+    }, {
+        title: "Library Management System",
+        description: "Library management  website which can manage users and books and also users can reserve books.",
+        img: "/images/lms.png",
+        link: "https://github.com/MininduSenadheera/Library-Management-System"
+    }]
+
     return (
         <div id="projects" className="container my-5 grid" >
             <h1 className="title" data-aos="fade-up">Check Out My Projects</h1>
             <main className="page-content-projects my-5">
-                <div className="projectBox" data-aos="fade-up">
-                    <img src='/images/fieldr.png' className='img-fluid mb-4' alt='fleidr' />
-                    <h4>FieldR</h4>
-                    <p className="copy">(MERN Stack - Inprogress) <br /> Online cricket analytics and digital score sheet mobile app.</p>
-                    <Button
-                        href="https://community.fieldr.lk"
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<LanguageIcon />}>
-                        View site
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="200">
-                    <img src='/images/sling.png' className='img-fluid mb-4' alt='slingmobility' />
-                    <h4>slingmobility.com</h4>
-                    <p className="copy">(React Typescript Serverless) <br /> Affordable and innovative 360 degree EV solutions and fleet management.</p>
-                    <Button
-                        href="https://slingmobility.com"
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<LanguageIcon />}>
-                        View site
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="300">
-                    <img src='/images/aenigme.png' className='img-fluid mb-4' alt='aenigm3labs' />
-                    <h4>aenigm3labs.com</h4>
-                    <p className="copy">(React Typescript Serverless) <br /> Aenigme provides digital strategies for products & small and medium enterprises.</p>
-                    <Button
-                        href="https://aenigm3labs.com"
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<LanguageIcon />}>
-                        View site
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up">
-                    <img src='/images/Airbnb Price Predictor.png' className='img-fluid mb-4' alt='DWBI Airbnb' />
-                    <h4>Airbnb Price predictor</h4>
-                    <p className="copy">(Machine Learning, Data Mining) <br /> Predicting price of Airbnb hotel listings in Boston based on facilities provided.</p>
-                    <Button
-                        href='https://airbnb-price-prediction.streamlitapp.com'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<LanguageIcon />}>
-                        View Site
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="200">
-                    <img src='/images/DWBIAirbnb.png' className='img-fluid mb-4' alt='DWBI Airbnb' />
-                    <h4>DWBI for Airbnb Boston</h4>
-                    <p className="copy">(SSIS, SSAS, SSRS) <br /> Data Warehousing and Business Intelligence project done for Airbnb hotel listings in Boston.</p>
-                    <Button
-                        href='https://github.com/MininduSenadheera/DWBI-Airbnb-Boston'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="300">
-                    <img src='/images/DWBIAirbnb.png' className='img-fluid mb-4' alt='DWBI grocery' />
-                    <h4>DWBI for Grocery shop</h4>
-                    <p className="copy">(SSIS, SSAS, SSRS) <br /> Data Warehousing and Business Intelligence project done for a grocery shop.</p>
-                    <Button
-                        href='https://github.com/MininduSenadheera/DWBI-Grocery-Shop'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up">
-                    <img src='/images/AspirusHealthCare.png' className='img-fluid mb-4' alt='aspirus webapp' />
-                    <h4>Aspirus Health Care Web Application</h4>
-                    <p className="copy">(MERN Stack) <br /> Website for channelling doctors, e-prescriptions, telemedicine with live video audio conference and pharmacy store.</p>
-                    <Button
-                        href='https://github.com/JMAT-Technologies/Aspirus-Health-Care-WebApp'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="200">
-                    <img src='/images/AspirusAndroid.png' className='img-fluid mb-4' alt='aspirus android app' />
-                    <h4>Aspirus Health Care Android App</h4>
-                    <p className="copy">(Java, Firebase) <br /> Android app for channelling doctors, e-prescriptions, telemedicine with live video audio conference and pharmacy store.</p>
-                    <Button
-                        href='https://github.com/JMAT-Technologies/Aspirus-Health-Care-Android-App'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up" data-aos-delay="300">
-                    <img src='/images/BusTicketReservation.png' className='img-fluid mb-4' alt='bus ticket reservation web app' />
-                    <h4>Bus Ticket Reservation System</h4>
-                    <p className="copy">(JSP, SQL) <br /> A bus ticket reservation application where users can reserve seats online and manage their reservations.</p>
-                    <Button
-                        href='https://github.com/MininduSenadheera/Bus-Ticket-Reservation'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
-                <div className="projectBox" data-aos="fade-up">
-                    <img src='/images/lms.png' className='img-fluid mb-4' alt='library management system' />
-                    <h4>Library Management System</h4>
-                    <p className="copy">(PHP, SQL) <br /> Library management  website which can manage users and books and also users can reserve books.</p>
-                    <Button
-                        href='https://github.com/MininduSenadheera/Library-Management-System'
-                        target='_blank'
-                        variant="contained"
-                        endIcon={<GitHubIcon />}>
-                        View Project
-                    </Button>
-                </div>
+                {projects.map((project, index) =>
+                    <div className="projectBox" data-aos="fade-up" data-aos-delay={index * 100}>
+                        <img src={project.img} className='img-fluid mb-4' style={{ borderRadius: '8px' }} alt={project.title} />
+                        <h4>{project.title}</h4>
+                        <p>{project.description}</p>
+                        <Button
+                            href={project.link}
+                            target='_blank'
+                            variant="contained"
+                            endIcon={<LanguageIcon />}>
+                            View site
+                        </Button>
+                    </div>
+                )}
             </main>
         </div>
     )
