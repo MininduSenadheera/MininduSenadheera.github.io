9b2b98640afc634fd8b52f0fd9da39dd9971c037
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-02-20 22:40:35 +0530

    skills hidden

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import './App.css';
 import Header from './components/Header/Header'
 import Home from './components/Home/Home';
 import About from './components/About/About'
-import Footer from './components/Footer/Footer';
 import Projects from './components/Projects/Projects';
 import Certificates from './components/Certificates/Certificates';
-import Skills from './components/Skills/Skills';
+// import Skills from './components/Skills/Skills';
 import Services from './components/Services/Services';
 import ScrollToTop from 'react-scroll-to-top';
 
@@ -18,11 +17,10 @@ function App() {
           <Header />
           <Home />
           <About />
-          <Skills />
+          {/* <Skills /> */}
           <Services />
           <Projects />
           <Certificates />
-          <Footer />
           <ScrollToTop smooth svgPath='M165,0C74.019,0,0,74.019,0,165s74.019,165,165,165s165-74.019,165-165S255.981,0,165,0z M255.606,205.606
 	C252.678,208.535,248.839,210,245,210s-7.678-1.464-10.606-4.394l-69.396-69.393l-69.392,69.393c-5.857,5.858-15.355,5.858-21.213,0
 	c-5.858-5.857-5.858-15.355,0-21.213l79.998-80c2.813-2.813,6.628-4.394,10.606-4.394c3.979,0,7.793,1.58,10.607,4.394l80.002,80
