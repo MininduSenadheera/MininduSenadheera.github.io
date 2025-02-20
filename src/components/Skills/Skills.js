c7fbe065dc924648b2f687ec7db92ed05a591de9
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2025-02-20 22:40:21 +0530

    soft skills removed

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,29 +1,8 @@
 import React from 'react'
 import './Skills.css'
-import SoftSkill from './SoftSkill';
 import Languages from './Languages';
 
 function Skills() {
-	const softSkills = [{
-		skill: "Communication",
-		rating: 90
-	}, {
-		skill: "Creativity",
-		rating: 80
-	}, {
-		skill: "Team Work",
-		rating: 95
-	}, {
-		skill: "Leadership",
-		rating: 75
-	}, {
-		skill: "Problem Solving",
-		rating: 90
-	}, {
-		skill: "Positive Attitude",
-		rating: 80
-	}]
-
 	const languages = [{
 		language: "Typescript",
 		rating: 95
@@ -56,14 +35,6 @@ function Skills() {
 						<div className='boxAbout'>
 							{languages.map((language, index) => 
 								<Languages key={index} rating={language.rating} language={language.language} />
-							)}
-						</div>
-					</div>
-					<div className='col-xl-7 mb-5'>
-						<h3 className='subtitle mb-3'>Soft Skills</h3>
-						<div className='page-content-skills'>
-							{softSkills.map((skill, index) =>
-								<SoftSkill key={index} rating={skill.rating} skill={skill.skill} />
 							)}
 						</div>
 					</div>
